9324f8dccebc74fd14cb70a30ee3fffec68a69d3
Matthew-MBG <61323301+Matthew-MBG@users.noreply.github.com>
2021-12-02 20:35:12 +0100

    Update OCOS.java

--- src/main/java/me/matthew_mbg/ocos/OCOS.java
+++ src/main/java/me/matthew_mbg/ocos/OCOS.java
@@ -1,25 +1,55 @@
 package me.matthew_mbg.ocos;
 
-import java.util.List;
 import net.labymod.api.LabyModAddon;
-import net.labymod.settings.elements.BooleanElement;
-import net.labymod.settings.elements.ControlElement.IconData;
 import net.labymod.settings.elements.SettingsElement;
-import net.labymod.utils.Material;
 
-public class ExampleAddon extends LabyModAddon {
+import java.util.List;
 
-  @Override
-  public void onEnable() {
-  }
+public class OCOS {
+	
+	public String NAME =  "OCOS", VERSION = "1.0.0", NAMEVERSION = NAME + " " + VERSION, AUTHOR = "Matthew_MBG";
 
-  @Override
-  public void loadConfig() {
 
-  }
+/**
+ * Test addon
+ */
+public class OCOS extends LabyModAddon {
 
-  @Override
-  protected void fillSettings(List<SettingsElement> list) {
-    list.add(new BooleanElement("test", new IconData(Material.ACACIA_FENCE)));
-  }
+    /**
+     * Called when the addon gets enabled
+     */
+    @Override
+    public void onEnable() {
+      System.out.println("Starting " + NAMEVERSION + " by " + AUTHOR + ".");
+
+    }
+
+    /**
+     * Called when the addon gets disabled
+     */
+    @Override
+    public void onDisable() {
+      System.out.println("Shutting down " + NAME + ".");
+
+    }
+
+    /**
+     * Called when this addon's config was loaded and is ready to use
+     */
+    @Override
+    public void loadConfig() {
+      System.out.println(NAME + "config loaded.");
+
+    }
+
+    /**
+     * Called when the addon's ingame settings should be filled
+     *
+     * @param subSettings a list containing the addon's settings' elements
+     */
+    @Override
+    protected void fillSettings( List<SettingsElement> subSettings ) {
+
+    }
+
 }

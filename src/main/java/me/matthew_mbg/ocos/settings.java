ec3d9ca9c67c71a5e2847df5c41b4c8a9737c497
Matthew-MBG <61323301+Matthew-MBG@users.noreply.github.com>
2021-12-03 12:47:23 +0100

    Update settings.java

--- src/main/java/me/matthew_mbg/ocos/settings.java
+++ src/main/java/me/matthew_mbg/ocos/settings.java
@@ -1,38 +1,16 @@
 package me.matthew_mbg.ocos.settings;
 
-public class settings {
-    subSettings.add( new BooleanElement( "Enabled" /* Display name */, new ControlElement.IconData( Material.LEVER ), new Consumer<Boolean>() {
-        @Override
-        public void accept( Boolean accepted ) {
-            System.out.println( "Changed value: " + accepted );
-        }
-    } /* Change listener */, true /* current value */ ) );
-    
-    final DropDownMenu<EnumModuleAlignment> alignmentDropDownMenu = new DropDownMenu<EnumModuleAlignment>( "Custom Alignment" /* Display name */, 0, 0, 0, 0 )
-                    .fill( EnumModuleAlignment.values() );
-    DropDownElement<EnumModuleAlignment> alignmentDropDown = new DropDownElement<EnumModuleAlignment>( "Custom Alignment", alignmentDropDownMenu );
-    
-    // Set selected entry
-    alignmentDropDownMenu.setSelected( EnumModuleAlignment.DEFAULT );
-    
-    // Listen on changes
-    alignmentDropDown.setChangeListener( new Consumer<EnumModuleAlignment>() {
-        @Override
-        public void accept( EnumModuleAlignment alignment ) {
-            System.out.println( "New selected alignment: " + alignment.name() );
-        }
-    } );
+public enum PriorityCapeType {
+    LABYMOD("LabyMod"),
+    OPTIFINE("Optifine");
 
-    // Change entry design (optional)
-    alignmentDropDownMenu.setEntryDrawer( new DropDownMenu.DropDownEntryDrawer() {
-        @Override
-        public void draw( Object object, int x, int y, String trimmedEntry ) {
-            // We translate the value and draw it
-            String entry = object.toString().toLowerCase();
-            LabyMod.getInstance().getDrawUtils().drawString( LanguageManager.translate( entry ), x, y );
-        }
-    } );
-    
-    // Add to sublist
-    subSettings.add( alignmentDropDown );
+    private final String prioritycapeName;
+
+    private PriorityCapeType(String prioritycapeName) {
+        this.prioritycapeName = prioritycapeName;
+    }
+
+    public String getPriorityCapeName() {
+        return this.prioritycapeName;
+    }
 }

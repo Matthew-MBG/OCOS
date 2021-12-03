f1d81bfbf256f7be849ef9a589ec31931345f1be
Matthew-MBG <61323301+Matthew-MBG@users.noreply.github.com>
2021-12-03 07:00:46 +0100

    Update Mixin.java

--- src/main/java/me/matthew_mbg/ocos/mixin/Mixin.java
+++ src/main/java/me/matthew_mbg/ocos/mixin/Mixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
-public class ExampleMixin {
+public class Mixin {
 
   @Inject(method = "runTick", at = @At("HEAD"))
   private void injectRunTick(CallbackInfo ci) {
